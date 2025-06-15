553720c5593e3dc8eef3b470c8a8d4b589dc9d2f
diyor011 <104572938+diyor011@users.noreply.github.com>
2025-06-15 16:25:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+
+// let min = +prompt("Randomni min sonini kiriting:");
+// let max = +prompt("Randomni max sonini kiriting:");
+
+
+
+
+// let min = 1;
+// let max = 10;
+
+// let randomSon = Math.floor(Math.random() * (max - min + 1)) + min;
+// console.log(randomSon); // 1 va 10 orasida (ikkalasi ham kiradi)
+
+
+// let random = Math.floor(Math.random() * 6) + 1;
+// console.log("Tasir qilingan kub: " + random);
+// // 1 dan 6 gacha random son — tärä kub tashlash kabi
+
+
+
+alert("Xozir sizda random sonlar chiqishni boshledi😜")
+
+
+
+
+
+
+
+// let natija = randomSon * 2 ;
+
+
+let randomSon = Math.floor(Math.random() * 50) + 7;
+alert("Random orqali chiqqan son: " + randomSon);
+
+let kopaytirish  = confirm("Biz random sonni 2 ga kopaytirmoqchimiz va 2 ga bolmoqchimiz va yana 3 ga  kopaytirmoqchimiz😊")
+
+let natija = randomSon * 2;
+alert("Random sonning 2 baravari: " + natija);
+
+let natija2 = natija /2 ;
+alert("Kopaytirgandagi chiqqan sonni 2 ga bolingani: " + natija2);
+
+let natija3 = natija2 *3;
+alert(" Bolingandagi chiqqan sonni 3 ga kopaytirilgani: " + natija3);
+
+alert("Random orqali chiqqan son: "  + randomSon + " edi.\n"   +  
+    "Biz uni 2ga kopaytirib " + natija + " sonini chiqardik.\n"   +
+    "Kopaytirgandagi chiqqan sonni 2 ga bolib " + natija2 + " sonini chiqardik. \n" +
+    "Bolingandagi chiqqan sonni 3 ga kopaytirib " + natija3 + " sonini chiqardik.😁"
+)
+
+
+
